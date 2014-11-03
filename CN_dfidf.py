dff9245525c449050ce7bf75ba2fa870e7058564
qing dai <qing.dai@neusmart.com>
2014-11-03 08:51:50 +0800

    第一个版本

---
+++ CN_dfidf.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 28 17:40:42 2014
+
+@author: david.dai
+"""
+
+import datetime
+import time
+import sqlite3
+import os
+import codecs
+import re
+import jieba.posseg as pseg
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+
+def persist_chatlog(filename, conn):
+    with conn:
+        cur = conn.cursor()    
+        
+        f = codecs.open(filename,'r+','gbk')
+        for line in f.readlines():
+            if line.startwith():
+                msg=generateMsg(line)
+                cur.execute('insert into message() values (?,?)',(msg,))
+
+    
+def generateMsg(line):
+    s = line.split()
+    msgTime = datetime.datetime.strptime(s[0],"%Y年%m月%d日%X")        
+    n = re.search('^(gelnic简妮:)?.+?:'.decode('utf-8','ignore'),s[1])
+    who = n.group(0)
+    said = s[1][len(who):]
+    return Message(msgTime,who,said)    
+
+def JB_tokenizer():
+    words = pseg.cut(sentences)
+    for w in words:
+        #extract nouns &　verbs only
+        if(w.flag in ['n','v']):
+            w.word
+    
+#TODO: 1. traning 2. dateset 測試
+def get_cls_model(sentences, categories):
+    #extract features via jieba, only consider noun & verb
+    
+    #building pipeline
+    text_clf = Pipeline([('vect', CountVectorizer(tokenizer=JB_tokenizer )),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', MultinomialNB()),
+    ])
+    #return trained models
+    text_clf = text_clf.fit(JB_tokenizer,categories)
+    return text_clf
+
+#copy of https://docs.python.org/2/library/sqlite3.html
+class Message(object):
+    def __init__(self,msgTime,who,said):
+        self.msgTime, self.who, self.said = msgTime, who, said
+        
+    def __repr__(self):
+        return "on %s, %s said: %s" % self.msgTime, self.who, self.said
+    
+def adapt_message(msg):
+    return "%d;%s;%s" % (time.mktime(msg.msgTime.timetuple()), msg.who, msg.said)
+    
+def convert_messgae(s):    
+    msgTime, who, said= (s.split(";")[0],map(str,s.split(";")[1:2]))
+    return Message(msgTime, who, said)
+    
+#获取文件列表（该目录下放着100份文档）
+def getFilelist(path):
+    filelist = []
+    files = os.listdir(path)
+    for f in files:
+        if(f[0] == '.') :
+            pass
+    else:
+        filelist.append(f)
+        return filelist,path
+
+if __name__ == "__main__":
+    sqlite3.register_adapter(Message, adapt_message)
+    sqlite3.register_converter("message", convert_messgae)
+    conn = sqlite3.connect("CN_CLS")
+    
+    #從目錄下读取文档
+    for filename in getFilelist("./chatlog"):
+        persist_chatlog( filename,conn )
+    
+    #模拟写入category    
+    
+    
+    #build models
+    clsModel = get_cls_model(sentences, categories)
+    
+    #evaluate the model
+    clsModel.predict()
+    
+    
+    
